94a72a9781acc00ee7d6c71731be22b357b719bf
IJEOGU KELECHI CHARLES <76209424+charlson91@users.noreply.github.com>
2022-06-23 16:32:31 +0100

    Add files via upload
    Newly created javascript file

---
+++ main.js
@@ -0,0 +1,6 @@
+let myName = "Charles Ijeogu";
+let myHeight = 1.7;
+let myCountry = "Nigeria";
+
+document.write("My name is : "+myName +"<br>"+" "+ "My height is: "+myHeight +"<br>" +" " +"My country is : "+myCountry +" ")
+
